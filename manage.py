fafa0bf7fd600001eee54d37bd0fd39638b27daa
ABHISHEK SETH <ritaseth2014@gmail.com>
2026-09-22 23:00:19 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative task..s."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'civic_issue_project.settings')
     try:
         from django.core.management import execute_from_command_line
